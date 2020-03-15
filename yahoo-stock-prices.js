f42b2649ed316b85eb0e5cc90b3496438fedbba3
Glaudson Silva <glaudsonsilva@gmail.com>
2020-03-15 09:42:48 -0300

    error handling

--- yahoo-stock-prices.js
+++ yahoo-stock-prices.js
@@ -1,41 +1,37 @@
-var request = require('request');
+const request = require('request');
 
-module.exports.getHistoricalPrices = function(startMonth, startDay, startYear, endMonth, endDay, endYear, ticker, frequency, callback){
+module.exports.getHistoricalPrices = function (startMonth, startDay, startYear, endMonth, endDay, endYear, ticker, frequency, callback) {
 
-	var startDate = Math.floor(Date.UTC(startYear, startMonth, startDay, 0, 0, 0)/1000);
+	const baseUrl = 'https://finance.yahoo.com/quote/';
+	const startDate = Math.floor(Date.UTC(startYear, startMonth, startDay, 0, 0, 0) / 1000);
+	const endDate = Math.floor(Date.UTC(endYear, endMonth, endDay, 0, 0, 0) / 1000);
 
-	var endDate = Math.floor(Date.UTC(endYear, endMonth, endDay, 0, 0, 0)/1000);
+	request(baseUrl + ticker + "/history?period1=" + startDate + "&period2=" + endDate + "&interval=" + frequency + "&filter=history&frequency=" + frequency, function (err, res, body) {
 
-	request("https://finance.yahoo.com/quote/" + ticker + "/history?period1=" + startDate + "&period2=" + endDate + "&interval=" + frequency + "&filter=history&frequency=" + frequency, function(err, res, body){
+		if (err) { callback(err); }
 
-		if (err) {
+		try {
+			var prices = JSON.parse(body.split('HistoricalPriceStore\":{\"prices\":')[1].split(",\"isPending")[0]);
 
-			callback(err);
-
+			callback(null, prices)
+		} catch (err) {
+			callback(err)
 		}
-
-		var prices = JSON.parse(body.split('HistoricalPriceStore\":{\"prices\":')[1].split(",\"isPending")[0]);
-
-		callback(null, prices)
-
 	});
-
 };
 
-module.exports.getCurrentPrice = function(ticker, callback){
+module.exports.getCurrentPrice = function (ticker, callback) {
 
-	request("https://finance.yahoo.com/quote/" + ticker + "/", function(err, res, body){
+	request(baseUrl + ticker + "/", function (err, res, body) {
 
-		if (err) {
+		if (err) { callback(err); }
 
-			callback(err);
+		try {
+			var price = parseFloat(body.split("currentPrice")[1].split("fmt\":\"")[1].split("\"")[0]);
 
+			callback(null, price);
+		} catch (err) {
+			callback(err)
 		}
-
-		var price = parseFloat(body.split("currentPrice")[1].split("fmt\":\"")[1].split("\"")[0]);
-
-		callback(null, price);
-
 	});
-
 };
