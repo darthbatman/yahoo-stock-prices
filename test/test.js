dbdbfcc817c0c6bb69517585507368d361527fd3
Rishi Masand <masandrishi@gmail.com>
2020-03-16 21:24:22 -0400

    test reformatting

--- test/test.js
+++ test/test.js
@@ -1,13 +1,9 @@
 var yahooStockPrices = require('../yahoo-stock-prices');
 
 yahooStockPrices.getHistoricalPrices(3, 2, 2016, 3, 9, 2016, 'JNJ', '1d', function(err, prices){
-
 	console.log(prices);
-
 });
 
 yahooStockPrices.getCurrentPrice('AAPL', function(err, price){
-
 	console.log(price);
-
 });
